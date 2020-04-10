e1918876665a97a40a7bcb4f9b484e9e9354a27f
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-04-10 20:01:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key1, value1|
+    value1.each do |key2, value2|
+      value2.each do |value3|
+         if pigeon_list.key? value3
+           pigeon_list[value3] = {}
+         end
+         if pigeon_list[value3].key? key1
+           pigeon_list[value3][key1] = []
+         end
+         pigeon_list[value3][key1].push(key2.to_s)
+      end
+    end
+  end
+  return pigeon_list
 end
