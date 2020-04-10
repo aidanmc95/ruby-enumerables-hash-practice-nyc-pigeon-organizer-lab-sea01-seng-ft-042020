dd60c6b597099ac85313263b0f8923cef1797d65
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-04-10 20:09:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,13 +3,14 @@ def nyc_pigeon_organizer(data)
   data.each do |key1, value1|
     value1.each do |key2, value2|
       value2.each do |value3|
-        binding.pry
         if pigeon_list[value3]
           pigeon_list[value3] = {}
         end
+        binding.pry
         if pigeon_list[value3][key1]
           pigeon_list[value3][key1] = []
         end
+        binding.pry
         pigeon_list[value3][key1].push(key2.to_s)
       end
     end
